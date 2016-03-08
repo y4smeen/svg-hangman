695031c357e4eef3bb030e1cee090e11601dcc60
Helen Li <helen82198@gmail.com>
2016-03-08 10:09:48 -0500

    starting with letters

---
+++ hangman.js
@@ -0,0 +1,39 @@
+var pic = document.getElementById("vimage");
+
+var getA = document.getElementById("a");
+var getB = document.getElementById("b");
+var getC = document.getElementById("c");
+var getD = document.getElementById("d");
+var getE = document.getElementById("e");
+var getF = document.getElementById("f");
+var getG = document.getElementById("g");
+var getH = document.getElementById("h");
+var getI = document.getElementById("i");
+var getJ = document.getElementById("j");
+var getK = document.getElementById("k");
+var getL = document.getElementById("l");
+var getM = document.getElementById("m");
+var getN = document.getElementById("n");
+var getO = document.getElementById("o");
+var getP = document.getElementById("p");
+var getQ = document.getElementById("q");
+var getR = document.getElementById("r");
+var getS = document.getElementById("s");
+var getT = document.getElementById("t");
+var getU = document.getElementById("u");
+var getW = document.getElementById("w");
+var getX = document.getElementById("x");
+var getY = document.getElementById("y");
+var getZ = document.getElementById("z");
+
+getA.addEventListener("click",typeA);
+
+var typeA = function(e){
+    typeLetter("a");
+}
+
+var typeLetter = function(letter){
+    console.log(letter);
+}
+
+
