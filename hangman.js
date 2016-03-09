7b01400030051afd198487d27fe9f117be7b918f
Yasmeen Roumie <y4smeen@gmail.com>
2016-03-09 10:12:14 -0500

    fix letters

--- hangman.js
+++ hangman.js
@@ -1,39 +1,60 @@
+var letters = ['a','b','c','d','e','f','g','h','i',
+              'j','k','l','m','n','o','p','q','r',
+              's','t','u','v','w','x','y','z']
+
 var pic = document.getElementById("vimage");
 
-var getA = document.getElementById("a");
-var getB = document.getElementById("b");
-var getC = document.getElementById("c");
-var getD = document.getElementById("d");
-var getE = document.getElementById("e");
-var getF = document.getElementById("f");
-var getG = document.getElementById("g");
-var getH = document.getElementById("h");
-var getI = document.getElementById("i");
-var getJ = document.getElementById("j");
-var getK = document.getElementById("k");
-var getL = document.getElementById("l");
-var getM = document.getElementById("m");
-var getN = document.getElementById("n");
-var getO = document.getElementById("o");
-var getP = document.getElementById("p");
-var getQ = document.getElementById("q");
-var getR = document.getElementById("r");
-var getS = document.getElementById("s");
-var getT = document.getElementById("t");
-var getU = document.getElementById("u");
-var getW = document.getElementById("w");
-var getX = document.getElementById("x");
-var getY = document.getElementById("y");
-var getZ = document.getElementById("z");
+// window.onload = function(){
+//
+//
+// }
 
-getA.addEventListener("click",typeA);
+var makeLetters = function(){
+  buttons = document.getElementById('buttons');
 
-var typeA = function(e){
-    typeLetter("a");
+  for (var i = 0; i < letters.length; i++){
+    newLetter = document.createElement('button');
+    newLetter.id = letters[i];
+    newLetter.innerHTML = letters[i];
+    buttons.appendChild(newLetter);
+  }
 }
+//
+// var getA = document.getElementById("a");
+// var getB = document.getElementById("b");
+// var getC = document.getElementById("c");
+// var getD = document.getElementById("d");
+// var getE = document.getElementById("e");
+// var getF = document.getElementById("f");
+// var getG = document.getElementById("g");
+// var getH = document.getElementById("h");
+// var getI = document.getElementById("i");
+// var getJ = document.getElementById("j");
+// var getK = document.getElementById("k");
+// var getL = document.getElementById("l");
+// var getM = document.getElementById("m");
+// var getN = document.getElementById("n");
+// var getO = document.getElementById("o");
+// var getP = document.getElementById("p");
+// var getQ = document.getElementById("q");
+// var getR = document.getElementById("r");
+// var getS = document.getElementById("s");
+// var getT = document.getElementById("t");
+// var getU = document.getElementById("u");
+// var getW = document.getElementById("w");
+// var getX = document.getElementById("x");
+// var getY = document.getElementById("y");
+// var getZ = document.getElementById("z");
 
-var typeLetter = function(letter){
-    console.log(letter);
-}
+// getA.addEventListener("click",typeA);
+//
+// var typeA = function(e){
+//     typeLetter("a");
+// }
+//
+// var typeLetter = function(letter){
+//     console.log(letter);
+// }
 
 
+makeLetters();
