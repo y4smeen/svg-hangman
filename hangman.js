3fea2bbadc241365cce2a55caba34c96dd6186ae
lihelennn <helen82198@gmail.com>
2016-03-10 22:10:16 -0500

    onclick stuff words

--- hangman.js
+++ hangman.js
@@ -1,12 +1,12 @@
 var pic = document.getElementById("vimage");
 
-var xcoor, ycoor, textElements;
+var xcoor, ycoor, textElements, textElementLength;
 
 var letters = ['a','b','c','d','e','f','g','h','i',
                'j','k','l','m','n','o','p','q','r',
                's','t','u','v','w','x','y','z']
 
-var words = ["hello", "bye", "Software Development"];
+var words = ["aaaaaaaaa", "Software Development"];
 
 var pic = document.getElementById("vimage");
 
@@ -16,15 +16,22 @@ var pic = document.getElementById("vimage");
 // }
 
 var makeLetters = function(){
+    var html;
     buttons = document.getElementById('buttons');
+    html = "<button id='play'>Play</button>";
+    buttons.innerHTML=html;
 
     for (var i = 0; i < letters.length; i++){
+	html += '<button onclick="typeLetter(\'' + letters[i] + '\');">'+letters[i] + '</button>';
+	/*
 	newLetter = document.createElement('button');
 	newLetter.id = letters[i];
 	newLetter.innerHTML = letters[i];
 	buttons.appendChild(newLetter);
+	*/
+	buttons.innerHTML=html;
     }
-    getButtons();
+    //getButtons();
 }
 
 
@@ -33,7 +40,7 @@ var getButtons = function(){
     var letters2 = buttons.getElementsByTagName("button"); 
     for (var i = 1; i < letters2.length; i++) { 
 	var letter = letters2[i].getAttribute("id"); 
-	letters2[i].addEventListener("click", function(){
+	letters2[i].addEventListener("click", function(e){
 	    var l = document.createElementNS("http://www.w3.org/2000/svg","text");
 	    l.setAttribute("x","50");
 	    l.setAttribute("y","50");
@@ -68,49 +75,57 @@ makeLetters();
   }
 
   getElement();
+*/
 
-  var getA = document.getElementById("a");
-  var getB = document.getElementById("b");
-  var getC = document.getElementById("c");
-  var getD = document.getElementById("d");
-  var getE = document.getElementById("e");
-  var getF = document.getElementById("f");
-  var getG = document.getElementById("g");
-  var getH = document.getElementById("h");
-  var getI = document.getElementById("i");
-  var getJ = document.getElementById("j");
-  var getK = document.getElementById("k");
-  var getL = document.getElementById("l");
-  var getM = document.getElementById("m");
-  var getN = document.getElementById("n");
-  var getO = document.getElementById("o");
-  var getP = document.getElementById("p");
-  var getQ = document.getElementById("q");
-  var getR = document.getElementById("r");
-  var getS = document.getElementById("s");
-  var getT = document.getElementById("t");
-  var getU = document.getElementById("u");
-  var getW = document.getElementById("w");
-  var getX = document.getElementById("x");
-  var getY = document.getElementById("y");
-  var getZ = document.getElementById("z");
+/*
+var getA = document.getElementById("a");
+var getB = document.getElementById("b");
+var getC = document.getElementById("c");
+var getD = document.getElementById("d");
+var getE = document.getElementById("e");
+var getF = document.getElementById("f");
+var getG = document.getElementById("g");
+var getH = document.getElementById("h");
+var getI = document.getElementById("i");
+var getJ = document.getElementById("j");
+var getK = document.getElementById("k");
+var getL = document.getElementById("l");
+var getM = document.getElementById("m");
+var getN = document.getElementById("n");
+var getO = document.getElementById("o");
+var getP = document.getElementById("p");
+var getQ = document.getElementById("q");
+var getR = document.getElementById("r");
+var getS = document.getElementById("s");
+var getT = document.getElementById("t");
+var getU = document.getElementById("u");
+var getW = document.getElementById("w");
+var getX = document.getElementById("x");
+var getY = document.getElementById("y");
+var getZ = document.getElementById("z");
 
-  getA.addEventListener("click",typeA);
+getA.addEventListener("click",typeA);
 
-  function typeA(e){
-  typeLetter(e);
-  }
+function typeA(e){
+    typeLetter("a");
+}
 
 */
-function typeLetter(letter){
-    var l = document.createElementNS("http://www.w3.org/2000/svg","text");
-    l.setAttribute("x","50");
-    l.setAttribute("y","50");
-    l.setAttribute("font-family","sans-serif");
-    l.setAttribute("font-size","20px");
-    l.textContent=letter;
-    pic.appendChild(l);
 
+function typeLetter(letter){
+    for (var i = 0; i<currentWord.length; i++){
+	if (currentWord[i] == letter){
+	    var l = document.createElementNS("http://www.w3.org/2000/svg","text");
+	    xcoor = 50 + 45*i + 5;
+	    ycoor = 47;
+	    l.setAttribute("x",xcoor.toString());
+	    l.setAttribute("y",ycoor.toString());
+	    l.setAttribute("font-family","sans-serif");
+	    l.setAttribute("font-size","20px");
+	    l.textContent=letter;
+	    pic.appendChild(l);
+	}
+    }
 }
 
 var play = document.getElementById("play");
@@ -120,17 +135,10 @@ function playFunc(){
     currentWord = words[Math.floor(Math.random() * words.length)]
     console.log(currentWord);
     textElements = document.getElementsByTagName("text");
-    console.log(textElements);
-    console.log("length: " + textElements.length);
-    for(var ii=0; ii<textElements.length; ii++){
-	pic.removeChild(pic.childNodes[ii]);
-	//textElements[ii].parentNode.removeChild(textElements[ii]);
+    textElementLength = textElements.length;
+    for(var ii=0; ii<textElementLength; ii++){
+	textElements[ii].textContent = "";
     }
-    textElements = document.getElementsByTagName("text");
-    console.log(textElements);
-
-    
-
 
     for (var i = 0; i < currentWord.length; i++){
 	if(currentWord[i] != " "){
