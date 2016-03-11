377023c648cd00fe773f26bed7a373d0b409118c
lihelennn <helen82198@gmail.com>
2016-03-10 22:16:50 -0500

    cleaning up a little

--- hangman.js
+++ hangman.js
@@ -2,11 +2,11 @@ var pic = document.getElementById("vimage");
 
 var xcoor, ycoor, textElements, textElementLength;
 
-var letters = ['a','b','c','d','e','f','g','h','i',
-               'j','k','l','m','n','o','p','q','r',
-               's','t','u','v','w','x','y','z']
+var letters = ['A','B','C','D','E','F','G','H','I',
+               'J','K','L','M','N','O','P','Q','R',
+               'S','T','U','V','W','X','Y','Z']
 
-var words = ["aaaaaaaaa", "Software Development"];
+var words = ["AAAAAA", "SOFTWARE DEVELOPMENT", "HELLO"];
 
 var pic = document.getElementById("vimage");
 
@@ -24,14 +24,13 @@ var makeLetters = function(){
     for (var i = 0; i < letters.length; i++){
 	html += '<button onclick="typeLetter(\'' + letters[i] + '\');">'+letters[i] + '</button>';
 	/*
-	newLetter = document.createElement('button');
-	newLetter.id = letters[i];
-	newLetter.innerHTML = letters[i];
-	buttons.appendChild(newLetter);
+	  newLetter = document.createElement('button');
+	  newLetter.id = letters[i];
+	  newLetter.innerHTML = letters[i];
+	  buttons.appendChild(newLetter);
 	*/
 	buttons.innerHTML=html;
     }
-    //getButtons();
 }
 
 
@@ -55,62 +54,7 @@ var getButtons = function(){
 
 makeLetters();
 
-//Fix this plz
 
-//makeLetters();
-//getButtons();
-
-
-/*
-  function getElement(){
-  for (var i = 0; i < letters.length; i++){
-  hello = document.getElementById(letters[i]);
-  hello.addEventListener("click",function(hello){
-  console.log(hello);
-  console.log(hello.innerHTML);
-  }
-  )
-
-  }
-  }
-
-  getElement();
-*/
-
-/*
-var getA = document.getElementById("a");
-var getB = document.getElementById("b");
-var getC = document.getElementById("c");
-var getD = document.getElementById("d");
-var getE = document.getElementById("e");
-var getF = document.getElementById("f");
-var getG = document.getElementById("g");
-var getH = document.getElementById("h");
-var getI = document.getElementById("i");
-var getJ = document.getElementById("j");
-var getK = document.getElementById("k");
-var getL = document.getElementById("l");
-var getM = document.getElementById("m");
-var getN = document.getElementById("n");
-var getO = document.getElementById("o");
-var getP = document.getElementById("p");
-var getQ = document.getElementById("q");
-var getR = document.getElementById("r");
-var getS = document.getElementById("s");
-var getT = document.getElementById("t");
-var getU = document.getElementById("u");
-var getW = document.getElementById("w");
-var getX = document.getElementById("x");
-var getY = document.getElementById("y");
-var getZ = document.getElementById("z");
-
-getA.addEventListener("click",typeA);
-
-function typeA(e){
-    typeLetter("a");
-}
-
-*/
 
 function typeLetter(letter){
     for (var i = 0; i<currentWord.length; i++){
