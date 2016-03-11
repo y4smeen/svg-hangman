4596a7a8c964f93b755edbe8c1de5b7ccbc2ee6c
Wayez <c.wayez3793@gmail.com>
2016-03-11 04:39:43 +0000

    added a few categories

--- hangman.js
+++ hangman.js
@@ -8,6 +8,32 @@ var letters = ['A','B','C','D','E','F','G','H','I',
 
 var words = ["AAAAAA", "SOFTWARE DEVELOPMENT", "HELLO"];
 
+var states = ["Alabama"," Alaska","Arizona",,"Arkansas",,"California","Colorado", 
+"Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana",
+"Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan",
+"Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire",
+"New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma",
+"Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee",
+"Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming",
+"Washington DC"];
+
+var foods = ["asparagus", "apples", "avacado", "acorn", "almond", "bacon", "beans",
+"bagels", "beer", "bread", "broccoli", "buritto", "cabbage", "cake", "carrots","celery",
+"cheese", "chicken", "catfish", "chips", "chocolate", "chowder", "clams", "coffee",
+"cookies", "corn", "cupcakes", "crab", "curry", "cereal", "dumplings", "donuts", "eggs",
+"eggrolls", "muffins", "sushi", "fajita", "falafel", "fish", " French toast", "granola",
+"grapes", "Guancamole", "Graham crackers", "ham", "hamburger", "honey", "hash browns",
+"hot dogs", "hummus", "ice cream", "jelly" , "kiwi", "lobster", "Lamb", "Linguine",
+"Lasagna", "Meatballs", "Milk", "Milkshake","Noodles","Pizza", "Peppers", "Pancakes",
+"Quesadilla", "Spinach", "Spaghetti", "Tater tots", "Toast", "Waffles", "Wine", "Yogurt"];
+
+var people = ["Aristotle", "Plato", "Jesus Christ", "Socrates", "Alexander the Great",
+"Leonardo da Vinci", "Confucius", "Julius Caesar", "Homer", "Pythagoras", "Archimedes",
+"Moses", "Muhammad", "Abraham", "Adolf Hitler", "Wolfgang Amadeus Mozart", "Charlemagne",
+"William Shakespeare", "Michelangelo", "Augustus", "Napoleon Bonaparte", "Isaac Newton",
+"Albert Einstein", "Christopher Columbus", "Johann Sebastian Bach",	
+"Ludwig van Beethoven", "Gautama Buddha", "Cleopatra", "Martin Luther", "Galileo Galilei",
+"Karl Marx", "Marco Polo", "Cicero", "Vincent van Gogh", "Genghis Khan"];	
 var pic = document.getElementById("vimage");
 
 // window.onload = function(){
@@ -76,7 +102,16 @@ var play = document.getElementById("play");
 play.addEventListener("click",playFunc);
 
 function playFunc(){
-    currentWord = words[Math.floor(Math.random() * words.length)]
+	var catRand = Math.floor(Math.random() * 3);
+	if (catRand == 0){
+		currentCat = states;
+	} else if (catRand == 1){
+		currentCat = foods;
+	} else {
+		currentCat = people;
+	}
+	console.log(currentCat);
+    currentWord = currentCat[Math.floor(Math.random() * currentCat.length)];
     console.log(currentWord);
     textElements = document.getElementsByTagName("text");
     textElementLength = textElements.length;
